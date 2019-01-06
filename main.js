54b6744a4f7ff92ac04b837f4718182a875fc5d8
AntonVoronezh <pstut@mail.ru>
2019-01-06 17:24:49 +0300

    lesson #8

--- main.js
+++ main.js
@@ -3,11 +3,24 @@ new Vue({
     data: {
         title: 'Hello',
         href: 'google.com',
-        link: '<a href="http://aaa.sss">link</a>'
+        link: '<a href="http://aaa.sss">link</a>',
+
+        example8: {
+            title: 'Hello 222',
+            coordX: 0
+        }
     },
+
     methods: {
         handleChange(event) {
-            this.title = event.target.value
+            this.title = event.target.value;
+        },
+        titleChange(newTitle) {
+            this.title = newTitle;
+        },
+        titleChange8(newTitle, event) {
+            this.example8.title = newTitle;
+            this.example8.coordX = event.clientX;
         }
     }
 });
