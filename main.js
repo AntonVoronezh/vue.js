e7417893af5259c22ccfe941b50da554bd0d6863
AntonVoronezh <pstut@mail.ru>
2019-01-06 15:26:40 +0300

    lesson #3

--- main.js
+++ main.js
@@ -2,5 +2,10 @@ new Vue({
     el: '#app',
     data: {
         title: 'Hello'
+    },
+    methods: {
+        handleChange(event) {
+            this.title = event.target.value
+        }
     }
 });
