c530752e674bbf1aac08756cd3af41fcd18b27bc
AntonVoronezh <pstut@mail.ru>
2019-01-06 16:12:14 +0300

    lesson #6

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ new Vue({
     el: '#app',
     data: {
         title: 'Hello',
-        href: 'google.com'
+        href: 'google.com',
+        link: '<a href="http://aaa.sss">link</a>'
     },
     methods: {
         handleChange(event) {
